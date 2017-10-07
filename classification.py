1fd2066b2ce4bc134bacdab64157cc1baf4a582f
mgildenhard <mgdatascience@posteo.de>
2017-10-07 20:01:39 +0200

    Initial commit

---
+++ classification.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# polistream
+__author__ = "Markus Gildenhard"
+__copyright__ = "Copyright 2017, Markus Gildenhard"
+__credits__ = ["Markus Gildenhard"]
+__license__ = ("MIT License as detailed in License File"
+                " provided with this software")
+__maintainer__ = "Markus Gildenhard"
+__email__ = "mgdatascience@posteo.de"
+__status__ = "Development"
+
+mnist = fetch_mldata('MNIST original')
